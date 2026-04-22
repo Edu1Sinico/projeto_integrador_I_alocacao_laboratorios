e36ba8c767f41e1e2198d4d67e21b55717577c55
Edu1Sinico <EduardoSenai888@gmail.com>
2026-04-22 17:09:42 -0300

    Realizando o teste de commit com o projeto - 22/04/2026

--- ProjetoIntegredor/dominio/entidades/ResponsavelTI.cs
+++ ProjetoIntegredor/dominio/entidades/ResponsavelTI.cs
@@ -2,11 +2,12 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ProjetoIntegredor.dominio.enums;
 
 namespace ProjetoIntegredor.model
 {
     public class ResponsavelTI : Usuario
     {
-        
+        public ResponsavelTI(int re, string nome, string senha, string email, TipoUsuario tipo) : base(re, nome, senha, email, tipo){}
     }
 }

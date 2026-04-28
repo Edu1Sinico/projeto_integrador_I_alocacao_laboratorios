1f0076f00cef30977f263931f7b5a338db446b77
Edu1Sinico <EduardoSenai888@gmail.com>
2026-04-27 22:05:34 -0300

    Dando inicio na interface gráfica - 27/04/2026

--- ProjetoIntegredor/Program.cs
+++ ProjetoIntegredor/Program.cs
@@ -1,6 +1,72 @@
 ﻿
-internal class Program{
-    public static void Main(string[] args){
+using ProjetoIntegredor.dominio.enums;
+using ProjetoIntegredor.model;
+using ProjetoIntegredor.Servicos;
 
+internal class Program
+{
+    public static void Main(string[] args)
+    {
+
+        // Funções do Service
+        UsuarioService user = new UsuarioService();
+
+        // Cadastrando o diretor
+        Usuario diretor = user.CadastrarUsuario(
+            "123",
+            "Marcos",
+            "123456",
+            "marcosdiretor@einstein.com.br",
+            TipoUsuario.DI
+        );
+
+        // Outros atributos
+        int? opcao = null;
+
+        // Login Usuario
+        string? re = null, senha = null, opcaoUser = null;
+        bool tipo = false;
+        TipoUsuario tipoUsuario;
+
+        do
+        {
+            // Login
+            Console.WriteLine("\n====== LOGIN ======\n");
+            // Tipo de Usuário
+            Console.Write("Selecione o usuário:"
+                + "\n1 - Diretor"
+                + "\n2 - coordenador"
+                + "\n3 - Responsável do TI");
+            opcaoUser = Console.ReadLine();
+
+            switch (Int32.Parse(opcaoUser))
+            {
+                case 1:
+                    tipoUsuario = TipoUsuario.DI;
+                    tipo = true;
+                    break;
+                case 2:
+                    tipoUsuario = TipoUsuario.CO;
+                    tipo = true;
+                    break;
+                case 3:
+                    tipoUsuario = TipoUsuario.RT;
+                    tipo = true;
+                    break;
+                default:
+                    Console.WriteLine("Informe um dos usuários!");
+                    tipo = false;
+                    break;
+            }
+
+            Console.WriteLine("Informe o RE Institucional: ");
+            re = Console.ReadLine();
+
+            Console.WriteLine("Informe a senha: ");
+            senha = Console.ReadLine();
+
+            user.Login(tipoUsuario, re, senha);
+
+        } while (!tipo);
     }
 }
